834c2f58ac8d1276610cb988ef0e0ce02c7e88db
Jacob Christie <j.christie@icloud.com>
2017-12-30 20:07:20 -0400

    Initial Commit

---
+++ USDAFoodCompositionDatabasesAPI/USDAFoodCompositionDatabasesAPI.h
@@ -0,0 +1,19 @@
+//
+//  USDAFoodCompositionDatabasesAPI.h
+//  USDAFoodCompositionDatabasesAPI
+//
+//  Created by Jacob Christie on 2017-12-30.
+//  Copyright © 2017 Jacob Christie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for USDAFoodCompositionDatabasesAPI.
+FOUNDATION_EXPORT double USDAFoodCompositionDatabasesAPIVersionNumber;
+
+//! Project version string for USDAFoodCompositionDatabasesAPI.
+FOUNDATION_EXPORT const unsigned char USDAFoodCompositionDatabasesAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <USDAFoodCompositionDatabasesAPI/PublicHeader.h>
+
+
